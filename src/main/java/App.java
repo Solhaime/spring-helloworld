14904867b6764b51b8aa7281651280bfb6493244
Mustafetov Beknur <mustafetovbr@gmail.com>
2020-11-14 11:41:22 +0300

    directed by Robert B. Weide

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,14 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld secondBean = applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat1.getMessage());
+        System.out.println(cat2.getMessage());
+        boolean firstComparison = bean == secondBean;
+        boolean secondComparison = cat1 == cat2;
+        System.out.println("Result of \"HelloWorld\" comparison is: " + firstComparison);
+        System.out.println("Result of comparison of two Cats is: " + secondComparison);
     }
 }
